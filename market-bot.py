46e8d67c252343d15843f902ab89b21995d4e720
Sajo8 <andrewsplin@protonmail.com>
2018-05-12 11:57:23 +0530

    lots of changes

--- market-bot.py
+++ market-bot.py
@@ -7,19 +7,19 @@ client = discord.Client()
 
 # list of words which triggers it
 bannedWords = ["trade", "exchange", "tradeogre", "sell turtlecoin", "buy turtlecoin", "sell trtl", "buy trtl", "tradesatoshi", "trde", "trae", "tradesat", 
-				"wen lambo", "wen moon", "trade-", "when moon", "when lambo", "price", "fiat", "pr1ce", "usd", "euro", "inr", "binance", "altcoin", "sats", 
+				"wen lambo", "wen moon", "trade-", "when moon", "when lambo", "fiat", "pr1ce", "usd", "euro", "inr", "binance", "altcoin", "sats", 
 				"to down", "ts down", "tradesatoshi down", "tradeogre down", "ogre down", "sats down", "exhcnage", "pump and dump", "dump", 
 				"eur", "gbp", "ruble", "but turtlecoin", "but trtl", "moons", "hitbtc", "trtl be worth", "trtl worth", "exhcnag,e"
 				"airdrop", "shitcoin", "shitcion", "shitconi", "gewi", "gwei", "mine and sell", "down trend", "resistance line", "bearish", "bullish",
 				"to da moon", "pump and dump", "pump & dump", "pumped and dumped", "pumped & dumped", "pump n dump", "pumped n dumped" , "cmc", "coin market cap",
-				"ico", "ipo", "stop loss", "mkid", "tether", "trading", "trend line", "buy wall", "sell wall", "buy order", "sell order", "buy trlt", "sell trlt", 
+				"ico", "ipo ", "stop loss", "mkid", "tether", "trading", "trend line", "buy wall", "sell wall", "buy order", "sell order", "buy trlt", "sell trlt", 
 				"wtb", "wts", "Bitfinex", "Kraken", "Coinbase", "coinone", "Bitstamp", "Bithumb", "Bittrex", "Quoine", "Gemini", "bitFlyer", "BTC-e", "Poloniex", 
 				"exx", "livecoin", "exmo", "korbit", "liqui", "cex.io", "tidex", "vaultoro", "itbit", "cryptopia", "yobit", "lakebtc", "kucoin", "btc-alpha", 
 				"quadrigacx", "localbitcoins", "coinfloor", "bitx", "coinexchange", "okcoin", "gatecoin", "coinmate", "bitso", "acx", "idex", "cryptobridge", 
 				"btcc", "bitfex", "virwox", "bleutrade", "abucoins", "paymium",  "flowbtc", "gate.io", "bitkonan", "cryptox", "cryptonit", "go4cryptos", "c-cex",
 				"novaexchange", "coincoz", "btctrade", "vircurex", "allcoin", "coinsecure",  "indacoin", "bitmarket", "braziliex", "coinnest", "dsx", 
 				"localturtlecoin", "localmonero", "local bitcoin", "local bitcoins", "localbitcoin", "stocks.exchange", "wen lamb0", "when lamb0", "wen l@mbo", 
-				"when l@mbo", "wen l@mb0", "when l@mb0", "indacion", "indaocin", "satoshi", "ogre", "tr@de", "tr2de", "elliot wave", "when buy in", "mt. gox",
+				"when l@mbo", "wen l@mb0", "when l@mb0", "indacion", "indaocin", "satoshis", "ogre", "tr@de", "tr2de", "elliot wave", "when buy in", "mt. gox",
 				" mt. gox", "mtgox", "mt gox", "pesos", "pesps", "trtl sell", "tlt sell", "turtlecoin sell", "trtl buy", "trlt buy", "turtlecoin buy",
 				"1sat", "1 sat", "buy turtle", "sell turtle", "crypto exchange", "cryptocurrency exchange", "crypto currency exchange", "sellin", "buyin",
 				"selin", "pumpin", "dumpin", "sellwall", "buywall", "selwall", " ts", "s@t", "0 sat", "0sat", "0stas", "stas", "st@s", "btc volume", 
@@ -27,13 +27,17 @@ bannedWords = ["trade", "exchange", "tradeogre", "sell turtlecoin", "buy turtlec
 				"buy bitcion", "buy btiocin", "buy bitocin", "coin offering", "inital coin offering", "crypto investing", "crypto invensting", "crypto investments",
 				"investment", "investment", "investing", "invensting", "dividends", "dividens", "profit, dividends and gains", "profit dividens and gains",
 				"mark3t", "xchange", "m@rket", "m@rk3t", "initial coin offering", "exchnage", "mkt", "xchnage", "bitfenix", "ccex", "exchamge", "buy some", "but some",
-				"bynance", "turtle cost", "cost turtle", "cost of turtle", "buy high sell low", "buy hi sell low", "sell high buy low", "sell hi buy low"]
+				"bynance", "turtle cost", "cost turtle", "cost of turtle", "buy high sell low", "buy hi sell low", "sell high buy low", "sell hi buy low", "trtl price", 
+				"price of trtl", "price trtl", "price of turtle", "turtle price", "price turtle", "price of rtrl", "coin price", "price of coin", "price coin", "2 sats",
+				"2 sat", "3 sat", "3 sats", "4 sat", "4 sats", "2sat", "3sat", "4sat", "5sat", "5 sat", "6sat", "6 sat", "7sat", "7 sat", "8sat", "8 sat",
+				"9sat", "9 sat", "price of the coin", "price of the trtl", "price of the turtle", "pricce for the coin", "price for the trtl", "price for the turtle",
+				"price for coin", "price for trtl", "price for turtle" ]
 
 # list of responses
-wordlist = ["Heyo, please move to the server linked in #406652012713738251>, we don't like it here.", 
-			"If you could move to the server linked in #406652012713738251>, that'd be great! We don't enjoy that here.", 
-			"Please move this discussion to the server linked in #406652012713738251>, we like to keep this server focused on support and development. Thank you! :t_smile:",
-			":rotating_light: MARKET TALK DETECTED :rotating_light: Please move to the server linked in #406652012713738251>, we don't like that here. :rotating_light:"]
+wordlist = ["Heyo, please move to the server linked in <#406652012713738251>, we don't like it here.", 
+			"If you could move to the server linked in <#406652012713738251>, that'd be great! We don't enjoy that here.", 
+			"Please move this discussion to the server linked in <#406652012713738251>, we like to keep this server focused on support and development. Thank you! <:t_smile:405478442599972874>",
+			":rotating_light: MARKET TALK DETECTED :rotating_light: Please move to the server linked in <#406652012713738251>, we don't like that here. :rotating_light:"]
 
 #help reply
 help = "Available commands - `^git` and `^todo`"
@@ -47,12 +51,11 @@ todo = "https://github.com/Sajo811/turtlecoin-market-linker/projects/1"
 # supreme power
 super = "WHEN WEB WALLET"
 
-# no permission to run
-perms = "Sorry, you don't have the privileges to run this command. It's like you're SuperMan, and this command is green kryptonite."
-
 # manual warn's response
-manres = "Hey, we don't like market-related talk in here. Join the server linked in #market-talk to discuss about it."
+manres = "Hey, we don't like market-related talk in here. Join the server linked in <#406652012713738251> to discuss about it."
 
+# tip me repsonse
+tip = "Hey, thanks for the thought!! If you tip me, it all goes to RainBorg. If you want to tip my creator, tip `@Sajo8#2953`. Once again, thank you! <:t_smile:405478442599972874>"
 
 @client.event 
 async def on_ready():
@@ -78,10 +81,6 @@ def shouldPrint():
 async def on_message(message):
 	# convert it to lower
 	msg = message.content.lower()
-
-	# check if the author is the **logged in** bot, in which case doesnt do anything
-	if(message.author.id == client.user.id):
-		return
 
 	# check if message incl. banned words. if so, spit a response
 	if any(word.lower() in msg for word in bannedWords):
@@ -115,9 +114,7 @@ async def on_message(message):
 
 	#commmand which can only be run by person whose user id matches this one here
 	if msg == "^super":
-		if message.author.id != "235707623985512451":
-			await client.send_message(message.channel, message.author.mention + " " + perms)
-		else:
+		if message.author.id == "235707623985512451":
 			await client.send_message(message.channel, message.author.mention + " " + super)
 		return  
 
@@ -129,4 +126,8 @@ async def on_message(message):
 			await client.send_message(message.channel, user.mention + " " + manres)
 	else:
 		return
+
+	if msg == "^tip":
+			await client.send_message(message.channel, message.author.mention + " " + tip)
+
 client.run("token")
